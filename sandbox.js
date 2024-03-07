af2e94a60834475afdc1d1d76ba13218f771f0be
jeeivan <jeeivansri@gmail.com>
2024-03-07 10:39:09 +0000

    Declaring variables, objects and arrays types

--- sandbox.js
+++ sandbox.js
@@ -1,25 +1,21 @@
-var names = ['luigi', 'mario', 'yoshi'];
-names.push('toad');
-var numbers = [10, 20, 30, 40];
-numbers.push(25);
-// numbers.push('shaun')
-var mixed = ['ken', 4, 'chun-li', 8, 9];
-mixed.push('ryu');
-mixed.push(10);
-mixed[0] = 3;
-// Objects
-var ninja = {
-    name: 'mario',
-    belt: 'black',
-    age: 30
-};
-ninja.age = 40;
-ninja.name = 'ryu';
-// ninja.age = '30'
-// ninja.skills = ['fighting', 'sneaking']
-ninja = {
-    name: 'yoshi',
-    belt: 'orange',
-    age: 40,
-    // skills: []
-};
+var charcater;
+var age;
+var isLoggedin;
+age = 30;
+isLoggedin = true;
+// arrays 
+var ninjas = [];
+ninjas.push('shuan');
+var mixed = [];
+mixed.push('hello');
+mixed.push(20);
+mixed.push(false);
+console.log(mixed);
+var uid;
+uid = '123';
+uid = 123;
+// objecs
+var ninjaOne;
+ninjaOne = { name: 'yoshi', age: 30 };
+var ninjaTwo;
+ninjaTwo = { name: 'mario', age: 20, beltColour: 'purple' };
--- sandbox.ts
+++ sandbox.ts
@@ -1,33 +1,34 @@
-let names = ['luigi', 'mario', 'yoshi']
+let charcater: string;
+let age: number;
+let isLoggedin: boolean;
 
-names.push('toad')
+age = 30;
 
-let numbers = [10, 20, 30, 40]
+isLoggedin = true
 
-numbers.push(25)
-// numbers.push('shaun')
+// arrays 
 
-let mixed = ['ken', 4, 'chun-li', 8, 9]
+let ninjas: string[] = [];
+ninjas.push('shuan');
 
-mixed.push('ryu')
-mixed.push(10)
-mixed[0] = 3;
+let mixed: (string|number|boolean)[] = []
+mixed.push('hello')
+mixed.push(20)
+mixed.push(false)
+console.log(mixed);
 
-// Objects
-let ninja = {
-    name: 'mario',
-    belt: 'black',
-    age: 30
+let uid: string|number;
+uid = '123'
+uid = 123
+
+// objecs
+let ninjaOne: object;
+ninjaOne = {name: 'yoshi', age: 30};
+
+let ninjaTwo: {
+    name: string,
+    age: number,
+    beltColour: string
 }
 
-ninja.age = 40
-ninja.name = 'ryu'
-// ninja.age = '30'
-// ninja.skills = ['fighting', 'sneaking']
-
-ninja = {
-    name: 'yoshi',
-    belt: 'orange',
-    age: 40,
-    // skills: []
-}
+ninjaTwo = {name: 'mario', age: 20, beltColour: 'purple'}
