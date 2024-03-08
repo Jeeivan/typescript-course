f88e13f8e3b3094faee91e9c9e7d88bfebc1d557
jeeivan <jeeivansri@gmail.com>
2024-03-08 11:38:40 +0000

    Using interfaces with classes

--- src/app.ts
+++ src/app.ts
@@ -1,44 +1,80 @@
+import { Invoice } from './classes/Invoice.js';
+import { Payment } from './classes/Payment.js';
+import { HasFormatter } from './interfaces/HasFormatter.js';
+
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
+
+// docOne = new Invoice('yoshi', 'web work', 250)
+// docTwo = new Payment('mario', 'plumbing work', 200)
+
+// let docs: HasFormatter[] = []
+// docs.push(docOne)
+// docs.push(docTwo)
+
+// console.log(docs);
+
+
+
 //interfaces
-interface IsPerson{
-    name: string;
-    age: number;
-    speak(a: string): void;
-    spend(a: number): number;
-}
+// interface IsPerson{
+//     name: string;
+//     age: number;
+//     speak(a: string): void;
+//     spend(a: number): number;
+// }
 
-const me: IsPerson = {
-    name: 'Shaun',
-    age: 30,
-    speak(text: string): void {
-        console.log(text);
-    },
-    spend(amount: number): number {
-        console.log('I spent', amount);
-        return amount
-    }
-}
+// const me: IsPerson = {
+//     name: 'Shaun',
+//     age: 30,
+//     speak(text: string): void {
+//         console.log(text);
+//     },
+//     spend(amount: number): number {
+//         console.log('I spent', amount);
+//         return amount
+//     }
+// }
 
-const greetPerson = (person: IsPerson) => {
-    console.log('hello', person.name);
+// const greetPerson = (person: IsPerson) => {
+//     console.log('hello', person.name);
     
-}
+// }
 
-greetPerson(me)
+// greetPerson(me)
 
-console.log(me);
+// console.log(me);
 
+// const invOne = new Invoice('mario', 'work on the mario website', 250)
+// const invTwo = new Invoice('luigi', 'work on the luigi website', 300)
 
+// let invoices: Invoice[] = []
+// invoices.push(invOne)
+// invoices.push(invTwo)
 
-import {Invoice} from './classes/Invoice.js'
-
-const invOne = new Invoice('mario', 'work on the mario website', 250)
-const invTwo = new Invoice('luigi', 'work on the luigi website', 300)
-
-let invoices: Invoice[] = []
-invoices.push(invOne)
-invoices.push(invTwo)
-
-invoices.forEach(inv => {
-    console.log(inv.client, inv.amount, inv.format());
+// invoices.forEach(inv => {
+//     console.log(inv.client, inv.amount, inv.format());
     
-})
+// })
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+console.log(form.children);
+
+// inputs
+const type = document.querySelector('#type') as HTMLInputElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault();
+
+  let doc: HasFormatter;
+  if (type.value === 'invoice'){
+    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber)
+  } else {
+    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)
+  }
+
+  console.log(doc)
+});
--- src/classes/Invoice.ts
+++ src/classes/Invoice.ts
@@ -1,9 +1,12 @@
+import { HasFormatter } from '../interfaces/HasFormatter.js'
+
+
 // classes
 // public is already default so anyone can change it
 // if private you cannot change property you cannot access it outside of the class
 // if readonly you can still access the property but you cannot change it
 
-export class Invoice {
+export class Invoice implements HasFormatter {
     // readonly client: string;
     // private details: string;
     // public amount: number;
