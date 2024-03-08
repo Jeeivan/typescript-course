9fb0a7f81449ac0ac25bd39f9ab4a3883bfdc633
jeeivan <jeeivansri@gmail.com>
2024-03-08 11:06:15 +0000

    Classes in ts

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,36 @@
-// const anchor = document.querySelector('a')!
+// classes
+class Invoice {
+    client: string;
+    details: string;
+    amount: number;
 
-// if(anchor){
-//     console.log(anchor.href);
-// }
+    constructor(c: string, d: string, a: number){
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    
+    format() {
+        return `${this.client} owes ${this.amount} for ${this.details}`
+    }
+}
 
-// console.log(anchor.href);
+const invOne = new Invoice('mario', 'work on the mario website', 250)
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300)
+
+let invoices: Invoice[] = []
+invoices.push(invOne)
+invoices.push(invTwo)
+
+invOne.client = 'yoshi'
+invTwo.amount = 400
 
 
-// const form = document.querySelector('form')!
+console.log(invoices);
+
+
+console.log(invOne, invTwo);
+
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement
 // console.log(form.children);
