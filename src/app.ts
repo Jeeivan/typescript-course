c55556581b1a3c61bc5601cdda648b2fa1b38a60
jeeivan <jeeivansri@gmail.com>
2024-03-08 15:29:46 +0000

    enums

--- src/app.ts
+++ src/app.ts
@@ -29,45 +29,24 @@ form.addEventListener('submit', (e: Event) => {
   list.render(doc, type.value, 'end')
 });
 
-// GENERICS
+// Enums
+enum ResourceType { BOOK, AUTHOR, FILM, DIRECTOR, PERSON}
 
-// const addUID = (obj: object) => {
-//   let uid = Math.floor(Math.random() * 100);
-//   return {...obj, uid};
-// }
-
-// const addUID = <T extends object>(obj: T) => {
-//   let uid = Math.floor(Math.random() * 100);
-//   return {...obj, uid};
-// }
-
-const addUID = <T extends {name: string}>(obj: T) => {
-    let uid = Math.floor(Math.random() * 100);
-    return {...obj, uid};
-  }
-  
-  let docOne = addUID({name: 'yoshi', age: 40});
-  //let docTwo = addUID('shaun');
-  
-  console.log(docOne.name);
-  
   // with interfaces
   interface Resource<T> {
     uid: number;
-    resourceName: string;
+    resourceType: number;
     data: T;
   }
   
-  const docThree: Resource<object> = {
+  const docOne: Resource<object> = {
     uid: 1, 
-    resourceName: 'person', 
-    data: { name: 'shaun' }
+    resourceType: ResourceType.BOOK,
+    data: { name: 'mario' }
+  };
+  const docTwo: Resource<object> = {
+    uid: 10, 
+    resourceType: ResourceType.PERSON,
+    data: { name: 'yoshi' }
   };
   
-  const docFour: Resource<string[]> = {
-    uid: 1, 
-    resourceName: 'shoppingList', 
-    data: ['bread', 'milk']
-  };
-  
-  console.log(docThree, docFour);
